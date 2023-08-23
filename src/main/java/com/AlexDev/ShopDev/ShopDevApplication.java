0f446c5d61aaea9876ea671c8680f72b500e5e0b
Alexey Kuriganov <wowo.vovo12345@yandex.ru>
2023-08-23 09:24:05 +0800

    add file website

---
+++ src/main/java/com/AlexDev/ShopDev/ShopDevApplication.java
@@ -0,0 +1,13 @@
+package com.AlexDev.ShopDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopDevApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ShopDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AlexDev/ShopDev/ShopDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AlexDev.ShopDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
